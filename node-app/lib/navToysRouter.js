23d34eb78137977db922f22e1524761f77c4cace
Sumit <sumittoshniwal92@gmail.com>
2017-07-10 06:50:10 +0530

    Adding search feature

--- node-app/lib/navToysRouter.js
+++ node-app/lib/navToysRouter.js
@@ -1,11 +1,11 @@
-var navBaseRouter = require(process.cwd() + '/lib/navBaseRouter.js')
+var navBaseRouter = require(process.cwd() + '/lib/navBaseRouter.js'),
     navLogUtil = require(process.cwd() + "/lib/navLogUtil.js"),
     navSystemUtil = require(process.cwd() + "/lib/navSystemUtil.js"),
     navResponseUtil = require(process.cwd() + "/lib/navResponseUtil.js"),
     navCommonUtil = require(process.cwd() + "/lib/navCommonUtil.js"),
     navValidationException = require(process.cwd() + "/lib/exceptions/navValidationException.js"),
     navMembershipExpirationException =  require(process.cwd() + "/lib/exceptions/navMembershipExpirationException.js"),
-    navLogicalException = require("node-exceptions").LogicalException;
+    navLogicalException = require("node-exceptions").LogicalException,
     navNoSubScriptionException = require(process.cwd() + "/lib/exceptions/navNoSubscriptionException.js"),
     navNoBalanceException = require(process.cwd() + "/lib/exceptions/navNoBalanceException.js"),
     navPendingReturnException = require(process.cwd() + "/lib/exceptions/navPendingReturnException.js"),
@@ -28,6 +28,7 @@ module.exports = class navToysRouter extends navBaseRouter {
         var self = this;
         this.router.use(this.ensureAuthenticated, this.ensureVerified, this.isSessionAvailable);
         this.router.get('/detail', function(req,res,next){self.getToysDetails(req,res,next)});
+        this.router.get('/search', function(req,res,next){self.getSearchPage(req,res,next)});
         this.router.get('/order', function(req,res,next){self.getOrder(req,res,next)});
         this.router.get('/orderPlaced', function(req,res,next){self.getOrderPlaced(req,res,next)});
         this.router.post('/placeOrder',(req, res, next) => {self.placeOrder(req,res,next)});
@@ -57,15 +58,15 @@ module.exports = class navToysRouter extends navBaseRouter {
                     user : req.user,
                     isLoggedIn : false,
                     layout : 'nav_bar_layout',
-            
+
                 });
                 /*response = new navResponseUtil().generateErrorResponse(error);
-                res.status(response.status).render("errorDocument",{
-                    errorResponse : response,
-                    user : req.user,
-                    isLoggedIn : false,
-                    layout : 'nav_bar_layout',
-                });*/
+                  res.status(response.status).render("errorDocument",{
+                  errorResponse : response,
+                  user : req.user,
+                  isLoggedIn : false,
+                  layout : 'nav_bar_layout',
+                  });*/
 
             })
         req.assert("id"," Bad Request").notEmpty();
@@ -80,13 +81,13 @@ module.exports = class navToysRouter extends navBaseRouter {
         }
         var navTDAO = new navToysDAO();
         navTDAO.getToyDetailById(id)
-        .then(function(toyDetail){
-            if(toyDetail.length == 0) {
-                return Q.reject(new navLogicalException());
-            }
-            toy = toyDetail;
-            return new navSystemUtil().getNoOfFilesMatchPat(toyDetail[0]._id+'_*',process.cwd() + '/../public/img/toys/');
-        })
+            .then(function(toyDetail){
+                if(toyDetail.length == 0) {
+                    return Q.reject(new navLogicalException());
+                }
+                toy = toyDetail;
+                return new navSystemUtil().getNoOfFilesMatchPat(toyDetail[0]._id+'_*',process.cwd() + '/../public/img/toys/');
+            })
         .done(function(result){
             return deferred.resolve(result);
         },function(error){
@@ -112,15 +113,15 @@ module.exports = class navToysRouter extends navBaseRouter {
                     user : req.user,
                     isLoggedIn : false,
                     layout : 'nav_bar_layout',
-            
+
                 });
                 /*response = new navResponseUtil().generateErrorResponse(error);
-                res.status(response.status).render("errorDocument",{
-                    errorResponse : response,
-                    user : req.user,
-                    isLoggedIn : false,
-                    layout : 'nav_bar_layout',
-                });*/
+                  res.status(response.status).render("errorDocument",{
+                  errorResponse : response,
+                  user : req.user,
+                  isLoggedIn : false,
+                  layout : 'nav_bar_layout',
+                  });*/
 
             })
         req.assert("id"," Bad Request").notEmpty();
@@ -168,15 +169,15 @@ module.exports = class navToysRouter extends navBaseRouter {
                     user : req.user,
                     isLoggedIn : false,
                     layout : 'nav_bar_layout',
-            
+
                 });
                 /*response = new navResponseUtil().generateErrorResponse(error);
-                res.status(response.status).render("errorDocument",{
-                    errorResponse : response,
-                    user : req.user,
-                    isLoggedIn : false,
-                    layout : 'nav_bar_layout',
-                });*/
+                  res.status(response.status).render("errorDocument",{
+                  errorResponse : response,
+                  user : req.user,
+                  isLoggedIn : false,
+                  layout : 'nav_bar_layout',
+                  });*/
 
             })
         req.assert("id"," Bad Request").notEmpty();
@@ -231,7 +232,7 @@ module.exports = class navToysRouter extends navBaseRouter {
         .then(function(){
             var membershipExpiry;
             if(userDetails.membership_expiry != null) {
-                 membershipExpiry = new navCommonUtil().getCurrentTime();
+                membershipExpiry = new navCommonUtil().getCurrentTime();
             }
             return new navUserDAO(rDAO.providedClient).updatePoints(user._id, (userDetails.balance) - (toyDetails.price), membershipExpiry);
         })
@@ -241,28 +242,28 @@ module.exports = class navToysRouter extends navBaseRouter {
         .catch(function(error){
 
             return rDAO.rollBackTx()
-                .then(function () {
-                    /*switch(error.name) {
-                        case "navNoBalanceException" :
-                            return Q.resolve({
-                                redirect : "/"
-                            })
-                            break;
-                        case "navNoSubscriptionException" :
-                            return Q.resolve({
-                                redirect : "/"
-                            })
-                            break;
-                        default:
-                            return Q.reject(error);
-                    }*/
-                    //res.status(500).send("Internal Server Error");
-                    return Q.reject(error);
-                })
-                .catch(function (err) {
-                    //log error
-                    return Q.reject(err);
-                })
+            .then(function () {
+                /*switch(error.name) {
+                  case "navNoBalanceException" :
+                  return Q.resolve({
+                  redirect : "/"
+                  })
+                  break;
+                  case "navNoSubscriptionException" :
+                  return Q.resolve({
+                  redirect : "/"
+                  })
+                  break;
+                  default:
+                  return Q.reject(error);
+                  }*/
+                //res.status(500).send("Internal Server Error");
+                return Q.reject(error);
+            })
+        .catch(function (err) {
+            //log error
+            return Q.reject(err);
+        })
         })
         .finally(function () {
             if (rDAO.providedClient) {
@@ -278,10 +279,134 @@ module.exports = class navToysRouter extends navBaseRouter {
         });
     }
     getOrderPlaced(req,res) {
-                res.render('orderPlaced',{
+        res.render('orderPlaced',{
+            user : req.user,
+            isLoggedIn : req.user ? true : false,
+            layout : 'nav_bar_layout'
+        });
+    }    
+    getSearchPage(req, res) {
+        var q = req.query.q, offset = req.query.offset, activeCategories = [], activeAgeGroups = []; 
+        console.log(req.query);
+        if(!offset ) {
+            offset = 0;
+        }
+        
+        for(var key in req.query) {
+            if((key == "category") && req.query.hasOwnProperty(key)) {
+                activeCategories.push(parseInt(req.query[key]) - 1);
+            }
+            if((key == "ageGroup") && req.query.hasOwnProperty(key)) {
+                activeAgeGroups.push(parseInt(req.query[key]) - 1);
+            }
+        }
+        
+        //req.assert("q"," Bad Request").notEmpty();
+        //req.assert("shippingAddress","Bad Request").notEmpty();
+        var deferred = Q.defer();
+        var respUtil =  new navResponseUtil(), toyList, categories, ageGroups, noOfPages, perPageToys = 4;
+        //console.log(repeatHelper);
+        deferred.promise
+            .done((result) => {
+                console.log(activeAgeGroups);
+                function genQueryParams() {
+                    var val = "";
+                    for(var i in req.query){
+                        if(req.query.hasOwnProperty(i) && i != "offset") {
+                            val += i + "=" + req.query[i] + "&"
+                        }
+                    }
+                    //val.charAt[val.length -1] = "";
+                    return val;
+                }
+                res.render('searchToys', {
                     user : req.user,
                     isLoggedIn : req.user ? true : false,
-                    layout : 'nav_bar_layout'
+                    layout : 'nav_bar_layout',
+                    query : q,
+                    queryParameters : genQueryParams(req.query),
+                    toysData : {
+                        toysList : toyList,
+                        filters : {
+                            categories : categories,
+                            ageGroups : ageGroups,
+                            activeCategories : activeCategories,
+                            activeAge : activeAgeGroups
+                        },
+                        noOfPages : noOfPages,
+                        perPageLimit : perPageToys,
+                        currentPage : offset ? (Math.floor(offset/perPageToys) + 1) : 1
+
+                    },
+                        helpers : {
+                            repeat : repeatHelper
+                        }
+
+                })    
+            },(error) => {
+                var response = respUtil.generateErrorResponse(error);
+                respUtil.renderErrorPage(req, res, {
+                    errorResponse : response,
+                    user : req.user,
+                    isLoggedIn : false,
+                    layout : 'nav_bar_layout',
+
                 });
-    }    
+                /*response = new navResponseUtil().generateErrorResponse(error);
+                  res.status(response.status).render("errorDocument",{
+                  errorResponse : response,
+                  user : req.user,
+                  isLoggedIn : false,
+                  layout : 'nav_bar_layout',
+                  });*/
+
+            })
+
+
+        var validationErrors = req.validationErrors();
+        //console.log(validationErrors);
+        var response, user = req.user;
+        if(validationErrors)
+        {
+            return deferred.reject(new navValidationError(validationErrors));
+        }
+        ageGroups = navCommonUtil.getAgeGroups();
+        categories = navCommonUtil.getCategories();
+        new navToysDAO().getAllToys(null, null, activeAgeGroups, activeCategories, q)
+            .then((toys) => {
+                toyList = [];
+                /*var temp = [];
+                for(var i in activeAgeGroups) {
+                    temp[activeAgeGroups[i]] = true;
+                }
+                activeAgeGroups = temp;
+                temp = [];
+                for(var i in activeCategories) {
+                    temp[activeCategories[i]] = true;
+                }
+                activeCategories = temp;*/
+                if(toys.length % perPageToys != 0 ) {
+                noOfPages = Math.floor(toys.length / perPageToys) + 1;
+                } else {
+                noOfPages = Math.floor(toys.length / perPageToys) ;
+
+                }
+                for(var i = 0; i < toys.length; i++) {
+                    if(i >= offset) {
+                        toyList.push(toys[i]);
+                    }
+                    if(toyList.length == perPageToys) {
+                        break;
+                    }
+                }
+                return Q.resolve();
+            })
+            .done(() => {
+                deferred.resolve();
+            }, (error) => {
+                deferred.reject(error);
+            });
+
+
+    }
 }
